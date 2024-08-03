2218cad5b211eab536634808308658b61bad33d4
bydlw98 <108581910+bydlw98@users.noreply.github.com>
2024-08-03 16:34:38 +0800

    test(windows): add tests for from_raw_psid trait method

--- src/os/windows/tests.rs
+++ src/os/windows/tests.rs
@@ -1,10 +1,27 @@
 use super::*;
 
+use std::ptr;
+
 #[test]
 fn test_groupidext_world() {
     let world_groupid = GroupidBuf::world().unwrap();
     let world_groupid_str = world_groupid.to_string();
-    println!("{:?}", world_groupid_str);
 
     assert_eq!(world_groupid_str, "S-1-1-0");
+}
+
+#[test]
+fn test_useridext_from_raw_psid_null() {
+    let psid: sys::PSID = ptr::null_mut();
+    let option = Userid::from_raw_psid(psid);
+
+    assert!(matches!(option, None));
+}
+
+#[test]
+fn test_groupidext_from_raw_psid_null() {
+    let psid: sys::PSID = ptr::null_mut();
+    let option = Groupid::from_raw_psid(psid);
+
+    assert!(matches!(option, None));
 }
